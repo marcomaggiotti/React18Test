d0fe7e70043a3c81699563668f9cdd92511de6e7
marcomaggiotti <52967417+marcomaggiotti@users.noreply.github.com>
2022-06-05 01:11:11 +0200

    Added boostrap and. tabs and buttons

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Product from "./Product";
+import Button from "react-bootstrap/Button";
 
 const ProductItem = ({ product, onAddToCartClicked }) => (
   <div style={{ marginBottom: 20 }}>
@@ -9,12 +10,17 @@ const ProductItem = ({ product, onAddToCartClicked }) => (
       price={product.price}
       quantity={product.inventory}
     />
-    <button
-      onClick={onAddToCartClicked}
-      disabled={product.inventory > 0 ? "" : "disabled"}
-    >
-      {product.inventory > 0 ? "Add to cart" : "Sold Out"}
-    </button>
+
+    <div className="component">
+      <Button
+        onClick={onAddToCartClicked}
+        disabled={product.inventory > 0 ? "" : "disabled"}
+        variant="primary"
+      >
+        {" "}
+        {product.inventory > 0 ? "Add to cart" : "Sold Out"}
+      </Button>
+    </div>
   </div>
 );
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ProductsContainer from "./ProductsContainer";
 import CartContainer from "./CartContainer";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => (
   <div>
     <h2>Shopping Cart Example</h2>
+    <Tabs
+      defaultActiveKey="profile"
+      id="uncontrolled-tab-example"
+      className="mb-3"
+    >
+      <Tab eventKey="home" title="Electronics"></Tab>
+      <Tab eventKey="profile" title="Crypto"></Tab>
+      <Tab eventKey="contact" title="Contact" disabled></Tab>
+    </Tabs>
     <hr />
+
     <ProductsContainer />
     <hr />
     <CartContainer />
