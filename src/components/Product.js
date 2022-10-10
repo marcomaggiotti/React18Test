4294e5b08382c07b49e1afacaa9f66f8cb1a909e
marcomaggiotti <52967417+marcomaggiotti@users.noreply.github.com>
2022-10-10 02:09:00 +0200

    round table added by quantity

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Offcanvas,
+  Form,
+  FormControl,
+  Button
+} from "react-bootstrap";
 import "./Product.css";
+
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) => <li>{number}</li>);
 
 const Product = ({ price, quantity, title }) => (
   <div>
     {title} - &#36;{price}
     {quantity ? ` x ${quantity}` : null}
-    <div class="circle">
-      <p class="text">Square text</p>
-    </div>
-    <div class="circle">
-      <p class="text">Circle text</p>
-    </div>
+    <Container class="btn-group" role="group" aria-label="Basic example">
+      {Array.from(Array(quantity), (e, i) => {
+        return (
+          <ul style={{ display: "inline-block" }}>
+            <li key={i}>
+              {i + 1}{" "}
+              <div class="circle_free">
+                <p class="text">Free Table</p>
+              </div>{" "}
+            </li>{" "}
+          </ul>
+        );
+      })}
+    </Container>
   </div>
 );
 
